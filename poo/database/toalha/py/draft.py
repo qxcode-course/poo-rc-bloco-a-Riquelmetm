a5a1baa3f6736e46c989315c64f88d56cff1d551
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-09-17 14:58:08 +0000

    Riquelme primeira atividade

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,8 @@
+class Towel:
+    def __init__ (self, color: str, size: str):
+        self.color: str = color
+        self.size: str = size
+        self.wetness: int = 0
+print("Qual a cor da sua toalha?")
+color = input()
+size = q
