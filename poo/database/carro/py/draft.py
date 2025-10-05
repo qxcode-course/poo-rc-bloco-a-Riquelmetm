34db51911bb3799aad990196bd37e829d353c1b4
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-05 03:12:52 +0000

    carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -5,4 +5,14 @@ class Carro:
         self.passMax = 2
         self.gas = 0
         self.gasMax = 100
+    def __str__(self) -> str:
+        return f"pass: {self.pass_}, gas: {self.gas}, km: {self.km}"
+    def enter(self):
+        self.pass_ +=1
+        if self.pass_ >= self.passMax:
+            print("fail: limite de pessoas atingido")
+    def leave(self):
+        self.pass_ -=1
+        if self.pass_ <= 0:
+            print("fail: nao ha ninguem no carro")
     
