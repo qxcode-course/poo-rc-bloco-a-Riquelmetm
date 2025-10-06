f458ae0c762c478e7745c36da0bb2ef1588f35e3
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-06 02:45:21 +0000

    carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -5,31 +5,36 @@ class Carro:
         self.passMax = 2
         self.gas = 0
         self.gasMax = 100
+
     def __str__(self) -> str:
         return f"pass: {self.pass_}, gas: {self.gas}, km: {self.km}"
+
     def enter(self):
-        self.pass_ +=1
         if self.pass_ >= self.passMax:
             print("fail: limite de pessoas atingido")
-            self.pass_ -=1
+        else:
+            self.pass_ += 1
+
     def leave(self):
-        self.pass_ -=1
-        if self.pass_ <= 0:
+        if self.pass_ == 0:
             print("fail: nao ha ninguem no carro")
-    def fuel_increment(self):
-        self.gas = int(input())
+        else:
+            self.pass_ -= 1
+
+    def fuel_increment(self, qtd: int):
+        self.gas += qtd
         if self.gas > self.gasMax:
-            self.gas = self.gasMax - self.gas
-    def drive_distance(self, distancia:int):
-        if self.pass_ <=0:
-            print("fail: não há ninguém no carro")
+            self.gas = self.gasMax
+
+    def drive_distance(self, distancia: int):
+        if self.pass_ == 0:
+            print("fail: nao ha ninguem no carro")
             return
-        if self.gas <=0:
+        if self.gas == 0:
             print("fail: tanque vazio")
             return
-        distancia = int(input())
-        if self.gas > distancia:
-            self.km = distancia
+        if self.gas >= distancia:
+            self.km += distancia
             self.gas -= distancia
         else:
             print(f"fail: tanque vazio apos andar {self.gas} km")
@@ -37,23 +42,22 @@ class Carro:
             self.gas = 0
 
 
-        
 def main():
     carro = Carro()
-    while True :
-        comando = input()
-        if comando == "end":
+    while True:
+        comando = input().strip().split()
+        if comando[0] == "end":
             break
-        elif comando == "enter":
+        elif comando[0] == "enter":
             carro.enter()
-        elif comando == "leave":
+        elif comando[0] == "leave":
             carro.leave()
-        elif comando == "show":
+        elif comando[0] == "show":
             print(carro)
-        elif comando == "fuel":
-            carro.fuel_increment()
-        elif comando == "drive":
-            carro.drive_distance()
+        elif comando[0] == "fuel":
+            carro.fuel_increment(int(comando[1]))
+        elif comando[0] == "drive":
+            carro.drive_distance(int(comando[1]))
 
 
-
+main()
